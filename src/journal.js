88d78768ff0bd2e47d9d0da3257285fe16b48d59
Scott Hutley <scotthutley1@comcast.net>
2021-08-30 16:35:52 -0700

    WIP getTeaser not working yet
    Co-authored-by: Erica Marroquin <ericamarroquin03@gmail.com>

--- src/journal.js
+++ src/journal.js
@@ -40,6 +40,51 @@ Entry.prototype.vowelFinder =  function() {
   return [vowelCount, consonantCount];
 }
 
+// Entry.prototype.getTeaser = function() {
+//   let spaceCounter = 0;
+//   for (let i=0; i < this.entry.length; i++) {
+//     if (this.entry[i] === " ") {
+//       if (spaceCounter === 8) {
+//         spaceIndex = this.entry.indexOf(this.entry[i]);
+//         teaserString = this.entry.substring(0, spaceIndex);
+//       } else {
+//         spaceCounter++;
+//       }
+//     }
+//   }
+//   return teaserString;
+// }
+
+
+Entry.prototype.getTeaser = function() {
+  let testEntry = [];
+  newEntry = testEntry.map(element =>  {
+    const splitEntry = element.split(" ");
+    const teaserString = splitEntry.slice(0, 8).join(" ");
+    return teaserString;
+  });
+  return newEntry;
+}
+
+
+const ingris =  [
+  "1 cup heavy cream",
+  "8 ounces paprika",
+  "1 Chopped Tomato",
+  "1/2 Cup yogurt",
+  "1 packet pasta ",
+  "1/2 teaspoon freshly ground black pepper, divided",
+];
+const shoppingList = ingris.map(ingredient => {
+  const splitIngredient = ingredient.split(' ');
+  const amount = splitIngredient.slice(0, 2).join(' ');
+  const val = splitIngredient.slice(2, splitIngredient.length).join(' ');
+  return { val, amount };
+});
+
+console.log(shoppingList);
+
+
 // Entry.prototype.consFinder = function() {
 //   const consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"];
 //   let consCount = 0;
